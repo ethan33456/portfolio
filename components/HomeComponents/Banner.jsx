80e29e8e0a42efccb33efb758901ef0afc47722a
Ethan Bell <wmmethan@gmail.com>
2025-11-24 17:53:28 -0600

    update resume and bio

--- components/HomeComponents/Banner.jsx
+++ components/HomeComponents/Banner.jsx
@@ -2,11 +2,11 @@ import Typewriter from 'typewriter-effect';
 
 const Banner = () => {
     return (
-        <div className="px-2 md:px-8 py-4 py-16">
+        <div className="px-9 md:px-8 py-4 py-16">
             <div className="flex flex-col">
                 <div className="flex flex-col gap-y-4">
                     <h1 className="text-Snow text-4xl lg:text-5xl font-bold mb-4">
-                        Hi, I'm Ethan Bell, You know
+                        Hi, I'm Ethan Bell 
                     </h1>
                     <div className="py-4 font-cascadia-normal text-Snow pb-4 text-xl lg:text-2xl">
                         <span className="text-Snow font-bold">
@@ -23,7 +23,7 @@ const Banner = () => {
                         </span>
                     </div>
                     <p className="text-Snow text-lg lg:text-xl mt-4 max-w-2xl">
-                        Lorem Ipsum Dolor Sit Amet Consectetur Adipisicing Elit.
+                        With substantial experience in a fast-paced agency setting, I have developed skills across a diverse range of technologies and software. Recognized for my innate problem-solving abilities, I excel at delivering efficient and straightforward solutions.
                     </p>
                 </div>
             </div>
--- components/HomeComponents/Expertise/MyExpertise.jsx
+++ components/HomeComponents/Expertise/MyExpertise.jsx
@@ -12,8 +12,8 @@ const MyExpertise = () => {
 
     return (
         <>
-            <div className="px-2 md:px-8 py-4 text-lg font-bold text-Snow">My Expertise</div>
-            <div className="grid justify items-center grid-flow-row md:grid-cols-2 lg:grid-cols-3 grid-rows-auto gap-4 px-2 md:px-8 " >
+            <div className="px-9 md:px-8 py-4 text-lg font-bold text-Snow">My Expertise</div>
+            <div className="grid justify items-center grid-flow-row md:grid-cols-2 lg:grid-cols-3 grid-rows-auto gap-4 px-9 md:px-8 " >
 
                 {
                     isLoading ?
